6b26c9a4ac1195c05ecc0607fef92df4a48a0b1c
Nisha Verma <nishverma11@gmail.com>
2017-07-07 00:05:17 +0530

    hierarchical json data manupulation

--- parse.py
+++ parse.py
@@ -1,11 +1,49 @@
 import json
+import re
 with open('out.json') as data_up:
 	data1=json.load(data_up)
-	fo=open('parsedFile.json','wb')
+	dict={}
+	dict['data']=[]
 	for r in data1['data']['data']:
-		fo.write('{')
-		fo.write('"name":"' + r['preferred_name'] +'",')
-		fo.write("\n")
-		fo.write('"id": ' + r['id'] + '},')
-		fo.write("\n")
-		print (r['preferred_name'])
+		s=r['manager']['url']
+		parentId=[""]
+		if s:
+			parentId=re.findall("D*(\d+)", s)
+				#print (parentId[0])
+			
+		dict['data'].append({
+			"name":r['preferred_name'] ,
+			"id":r['id'],
+			"manager":parentId[0],
+			"children":[]
+			})
+	
+with open('parsedFile.json', 'w') as outfile:  
+    data=json.dump(dict, outfile)
+    # print data
+
+with open('parsedFile.json') as json_data:
+	data1=json.load(json_data)
+	#data2=json.dumps(json.load(json_data))
+	dataMap={}
+	for r in data1['data']:
+		dataMap[r['id']]=r;
+	tree=[]
+	for r in data1['data']:
+		s=r['manager']
+		if s:
+			parent=dataMap[s]
+			children=parent['children']
+			children.append(r)
+			dataMap[s]['children']=children
+			parent['children']=children
+			if len(children)>0:
+				print dataMap[s]
+		else:
+			tree.append(dataMap[r['id']])
+	print("tree: ")
+	print tree
+	
+with open('finalJson.json', 'w') as outfile:  
+    data=json.dump(tree, outfile)
+    # print data
